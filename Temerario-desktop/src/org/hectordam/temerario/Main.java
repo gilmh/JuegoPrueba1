c5de46b43deea9a8f2f508e37a4b8409c1c43ae3
Hector Gil <gilmh93@gmail.com>
2014-02-14 11:17:36 +0100

    Proyecto recientemente creado

---
+++ Temerario-desktop/src/org/hectordam/temerario/Main.java
@@ -0,0 +1,16 @@
+package org.hectordam.temerario;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Temerario";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new Juego(), cfg);
+	}
+}
